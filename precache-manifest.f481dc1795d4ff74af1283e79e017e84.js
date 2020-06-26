9e3327e0bb70dca867a58760c72b20f147c55e91
vwhwang <vwhwang38@gmail.com>
2020-06-25 17:06:23 -0700

    Updates

---
+++ precache-manifest.f481dc1795d4ff74af1283e79e017e84.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1c3b0bbee4d577df53fec2409d29ec6",
+    "url": "/video-store-consumer/index.html"
+  },
+  {
+    "revision": "27eb292822f4fba4efff",
+    "url": "/video-store-consumer/static/css/main.331cdd72.chunk.css"
+  },
+  {
+    "revision": "d912e81339319b42d24f",
+    "url": "/video-store-consumer/static/js/2.ea915d24.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/video-store-consumer/static/js/2.ea915d24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27eb292822f4fba4efff",
+    "url": "/video-store-consumer/static/js/main.690aa688.chunk.js"
+  },
+  {
+    "revision": "4cad2615cad4c20889c0",
+    "url": "/video-store-consumer/static/js/runtime-main.888f3c54.js"
+  },
+  {
+    "revision": "f2bdc132c018ad1bb62f710935bb86a8",
+    "url": "/video-store-consumer/static/media/char_vic_v2.f2bdc132.jpg"
+  }
+]);
